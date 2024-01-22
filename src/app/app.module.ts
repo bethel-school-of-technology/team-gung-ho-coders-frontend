92fbe9e3aec49fe3eb119fb95d06af5e76d3bd3a
abigailtarpley <abigailtarpley@gmail.com>
2024-01-22 16:30:43 -0600

    Add Home button to movie page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { MoviePageComponent } from './movie-page/movie-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    MoviePageComponent
   ],
   imports: [
     BrowserModule,
