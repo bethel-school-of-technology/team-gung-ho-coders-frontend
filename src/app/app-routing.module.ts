ab0345396b429520ca8c7f81eb9fc7722a04bf00
abigailtarpley <abigailtarpley@gmail.com>
2024-01-27 19:03:53 -0600

    Update AboutUsComponent with new code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FavoritesComponent } from './components/favorites/favorites.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { MoviePageComponent } from './components/movie-page/movie-page.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {path: 'movies', component: MoviePageComponent},
   { path: 'favorites', component: FavoritesComponent },
   { path: '', component: SignInComponent },
+  { path: 'about-us', component: AboutUsComponent },
 ];
 
 @NgModule({
